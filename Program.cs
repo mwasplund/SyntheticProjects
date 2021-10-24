b447d2c1ceb378e1a31a7dc46fbe0884657795ef
Matthew Asplund <mwasplund@outlook.com>
2021-10-24 12:12:47 -0700

    Generate C# project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SimpleConsoleApplication
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World, Soup Style!");
+		}
+	}
+}
